0bf597ceafc56d0a7621b09eada878cea54c8f80
Dejon Gill <dejonjgill@gmail.com>
2025-11-01 23:31:58 -0400

    init
    Signed-off-by: Dejon Gill <dejonjgill@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get the flavor of Unix we're working with
+FLAVOR=$(uname -s)
+
+if [ "$FLAVOR" = "Darwin" ]; then
+  echo "Working on a Mac";
+  mac/install-brew.sh;
+  bash/install.sh
+elif [ "$FLAVOR" = "Linux" ]; then
+  echo "Working on Linux";
+  bash/install.sh
+else
+  echo "This script only works on Linux or Mac, not $FLAVOR";
+  exit 0;
+fi
